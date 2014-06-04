70bfdb9fe47d01b1176b250b2109af957586712f
mujahedsyed <msyed1983@gmail.com>
2014-06-04 08:12:53 +0100

    Java 8 Examples

---
+++ src/main/java/org/mujahed/org/mujahed/examples/functions/ConsumerFunEx.java
@@ -0,0 +1,24 @@
+package org.mujahed.org.mujahed.examples.functions;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Consumer Function Example
+ * Created by mujahed on 04/06/2014.
+ */
+public class ConsumerFunEx {
+
+    public static void main(String[] args){
+        Consumer<Integer> consumer = (Integer i) -> System.out.println(i);
+
+        forEach(Arrays.asList(1, 2, 3, 4, 5), consumer);
+    }
+
+    public static <T> void forEach(List<T> list, Consumer consumer){
+        for(T t: list){
+            consumer.accept(t);
+        }
+    }
+}
---
+++ src/main/java/org/mujahed/org/mujahed/examples/functions/FunctionFunEx.java
@@ -0,0 +1,25 @@
+package org.mujahed.org.mujahed.examples.functions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Created by mujahed on 04/06/2014.
+ */
+public class FunctionFunEx {
+
+    public static void main(String args[]){
+        map(Arrays.asList("Lambda", "in", "Action"), (String s)-> s.length());
+    }
+    public static <T, R> List<R> map(List<T> list,
+                                     Function<T, R> f) {
+        List<R> returnList = new ArrayList<>();
+        for(T t:list){
+           returnList.add(f.apply(t));
+        }
+
+        return returnList;
+    }
+}
